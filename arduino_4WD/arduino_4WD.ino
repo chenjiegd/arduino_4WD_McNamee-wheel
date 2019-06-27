92615444ea4c90cc2b9656cafba466074ce6adb8
chenjiegd <chenjiegd@foxmail.com>
2019-06-27 16:11:12 +0800

    Update arduino_4WD.ino

--- arduino_4WD/arduino_4WD.ino
+++ arduino_4WD/arduino_4WD.ino
@@ -3,6 +3,30 @@
 #include <Wire.h>
 #include <Adafruit_PWMServoDriver.h>
 #include <IRremote.h> //红外
+//本实验不可调节电机速度，调节PWM值会影响红外的信号接收
+
+//红外遥控按键码
+//FF00FF	开关
+//FF807F	前进
+//FF40BF	灯
+//FF20DF	左转
+//FFA05F	喇叭
+//FF609F	右转
+//FF10EF	左旋
+//FF906F	后退
+//FF50AF	右旋
+//FF30CF	加号
+//FFB04F	0
+//FF708F	减号
+//FF08F7	1
+//FF8877	2
+//FF48B7	3
+//FF28D7	4
+//FFA857	5
+//FF6897	6
+//FF18E7	7
+//FF9867	8
+//FF58A7	9
 
 Adafruit_PWMServoDriver pwm = Adafruit_PWMServoDriver(0x40);
 #define SERVOMIN 150 // this is the 'minimum' pulse length count (out of 4096)
@@ -34,8 +58,36 @@ enum
 char enServo[] = {0, 1, 2, 3};
 
 int key = 7;   //按键key
+int LED = 11;
 int Echo = 13; //Echo回声脚
 int Trig = 12; //Trig触发脚
+
+int RECV_PIN = A0; //定义红外接收器的引脚为2
+IRrecv irrecv(RECV_PIN);
+decode_results results;
+int on = 0; //标志位
+unsigned long last = millis();
+long flag_switch = 0xFF00FF;	 //开关
+long flag_run = 0xFF807F;		 //前进
+long flag_led = 0xFF40BF;		 //灯
+long flag_left = 0xFF20DF;		 //左转
+long flag_buzzer = 0xFFA05F;	 //喇叭
+long flag_right = 0xFF609F;		 //右转
+long flag_spin_left = 0xFF10EF;  //左旋
+long flag_back = 0xFF906F;		 //后退
+long flag_spin_right = 0xFF50AF; //右旋
+long flag_plus = 0xFF30CF;		 //加号
+long flag_zero = 0xFFB04F;		 //0
+long flag_minus = 0xFF708F;		 //减号
+long flag_one = 0xFF08F7;		 //1
+long flag_two = 0xFF8877;		 //2
+long flag_three = 0xFF48B7;		 //3
+long flag_four = 0xFF28D7;		 //4
+long flag_five = 0xFFA857;		 //5
+long flag_six = 0xFF6897;		 //6
+long flag_seven = 0xFF18E7;		 //7
+long flag_eight = 0xFF9867;		 //8
+long flag_nine = 0xFF58A7;		 //9
 
 /*小车初始速度控制*/
 int CarSpeed = 200;
@@ -60,33 +112,147 @@ void setup()
 
 	pwm.setPWMFreq(60); // Analog servos run at ~60 Hz updates
 
+	irrecv.enableIRIn(); // 初始化红外接收器
+
 	pinMode(key, INPUT); //定义按键输入脚
 	//初始化超声波引脚
 	pinMode(Echo, INPUT);  // 定义超声波输入脚
 	pinMode(Trig, OUTPUT); // 定义超声波输出脚
 
+	pinMode(LED, OUTPUT);
+	brake();
 	keysacn();
 }
 
 void loop()
 {
-	distance_test();
-	if (distance < 50)
+	//红外遥控
+	if (irrecv.decode(&results)) //调用库函数：解码
 	{
-		while (distance < 50)
+		if (millis() - last > 250) //确定接收到信号
+		{
+			on = !on; //标志位置反
+			dump(&results);
+		}
+		if (results.value == flag_switch)
 		{
 			/* code */
-			spin_right(100);
-			delay(20);
-			brake();
-			distance_test(); //测量前方距离
 		}
+		else if (results.value == flag_run)
+		{
+			/* code */
+			run(200);
+		}
+		else if (results.value == flag_led)
+		{
+			/* code */
+		}
+		else if (results.value == flag_left)
+		{
+			/* code */
+			left(200);
+		}
+		else if (results.value == flag_buzzer)
+		{
+			/* code */
+			brake();
+		}
+		else if (results.value == flag_right)
+		{
+			/* code */
+			right(200);
+		}
+		else if (results.value == flag_spin_left)
+		{
+			/* code */
+			spin_left(200);
+		}
+		else if (results.value == flag_back)
+		{
+			/* code */
+			back(200);
+		}
+		else if (results.value == flag_spin_right)
+		{
+			/* code */
+			spin_right(200);
+		}
+		else if (results.value == flag_plus)
+		{
+			/* code */
+		}
+		else if (results.value == flag_zero)
+		{
+			/* code */
+		}
+		else if (results.value == flag_minus)
+		{
+			/* code */
+		}
+		else if (results.value == flag_one)
+		{
+			/* code */
+		}
+		else if (results.value == flag_two)
+		{
+			/* code */
+		}
+		else if (results.value == flag_three)
+		{
+			/* code */
+		}
+		else if (results.value == flag_four)
+		{
+			/* code */
+		}
+		else if (results.value == flag_five)
+		{
+			/* code */
+		}
+		else if (results.value == flag_six)
+		{
+			/* code */
+		}
+		else if (results.value == flag_seven)
+		{
+			/* code */
+		}
+		else if (results.value == flag_eight)
+		{
+			/* code */
+		}
+		else if (results.value == flag_nine)
+		{
+			/* code */
+		}
+		else
+		{
+			/* code */
+		}
+		last = millis();
+		irrecv.resume();
 	}
-	else
-	{
-		/* code */
-		run(150); //无障碍物，前进
-	}
+
+	// spin_right(255);
+
+	// distance_test();
+	// if (distance < 50)
+	// {
+	// 	while (distance < 50)
+	// 	{
+	// 		/* code */
+	// 		spin_right(100);
+	// 		delay(20);
+	// 		brake();
+	// 		distance_test(); //测量前方距离
+	// 	}
+	// }
+	// else
+	// {
+	// 	/* code */
+	// 	run(150); //无障碍物，前进
+	// }
+
 	/* code */
 	// int Fdistance = 30;
 	// int Ldistance = 0, Rdistance = 0;
@@ -131,12 +297,12 @@ void loop()
 	// 	/* code */
 	// 	run(150); //无障碍物，前进
 	// }
-	
+
 	// Ultrasonic_avoid(50);
 
 	// Ultrasonic_avoid_servo(50);
 
-	// breathing_light(255, 10, 1);
+	breathing_light(255, 10, 1);
 
 	// set_breath_RGB(255);
 
@@ -452,13 +618,13 @@ void breathing_light(int brightness, int time, int increament)
 	for (int b = 0; b < brightness; b += increament)
 	{
 		int newb = map(b, 0, 255, 0, 4095);
-		pwm.setPWM(6, 0, newb);
+		pwm.setPWM(7, 0, newb);
 		delay(time);
 	}
 	for (int b = 255; b > 0; b -= increament)
 	{
 		int newb = map(b, 0, 255, 0, 4095);
-		pwm.setPWM(6, 0, newb);
+		pwm.setPWM(7, 0, newb);
 		delay(time);
 	}
 }
@@ -506,9 +672,9 @@ void distance_test() // 量出前方距离
 	delayMicroseconds(2);
 	digitalWrite(Trig, HIGH); // 给触发脚高电平10μs，这里至少是10μs
 	delayMicroseconds(10);
-	digitalWrite(Trig, LOW);			  // 持续给触发脚低电
+	digitalWrite(Trig, LOW);			   // 持续给触发脚低电
 	float Fdistance = pulseIn(Echo, HIGH); // 读取高电平时间(单位：微秒)
-	distance = Fdistance / 58;			  //为什么除以58等于厘米，  Y米=（X秒*344）/2
+	distance = Fdistance / 58;			   //为什么除以58等于厘米，  Y米=（X秒*344）/2
 	// X秒=（ 2*Y米）/344 ==》X秒=0.0058*Y米 ==》厘米=微秒/58
 	Serial.print("Distance:"); //输出距离（单位：厘米）
 	Serial.println(distance);  //显示距离
@@ -596,3 +762,37 @@ void distance_test() // 量出前方距离
 // 		run(150); //无障碍物，前进
 // 	}
 // }
+
+// void Infrared_avoid(){
+// 	//遇到障碍物,红外避障模块的指示灯亮,端口电平为LOW
+//     //未遇到障碍物,红外避障模块的指示灯灭,端口电平为HIGH
+//     int LeftSensorValue  = digitalRead(AvoidSensorLeft);
+//     int RightSensorValue = digitalRead(AvoidSensorRight);
+
+//     if (LeftSensorValue == HIGH && RightSensorValue == HIGH)
+//     {
+//       run();        //当两侧均未检测到障碍物时调用前进函数
+//     }
+//     else if (LeftSensorValue == HIGH && RightSensorValue == LOW)
+//     {
+//       spin_left(2); //右边探测到有障碍物，有信号返回，原地向左转
+//     }
+//     else if (RightSensorValue == HIGH && LeftSensorValue == LOW)
+//     {
+//       spin_right(2);//左边探测到有障碍物，有信号返回，原地向右转
+//     }
+//     else
+//     {
+//       spin_right(2);//当两侧均检测到障碍物时调用固定方向的避障(原地右转)
+//     }
+// }
+
+void dump(decode_results *results)
+{
+	int count = results->rawlen;
+	if (results->decode_type == UNKNOWN)
+	{
+		//Serial.println("Could not decode message");
+		brake();
+	}
+}
