fcab98638f5b78a3bb2495b404f26b08a8310dc6
chenjiegd <chenjiegd@foxmail.com>
2019-07-08 14:53:31 +0800

    Update arduino_4WD_WiFi-Camera.ino
    适配新版APP；
    优化左右舵机控制

--- WiFi-Camera-version/arduino_4WD_WiFi-Camera/arduino_4WD_WiFi-Camera.ino
+++ WiFi-Camera-version/arduino_4WD_WiFi-Camera/arduino_4WD_WiFi-Camera.ino
@@ -33,56 +33,61 @@ Adafruit_SSD1306 display(OLED_RESET);
 #error("Height incorrect, please fix Adafruit_SSD1306.h!");
 #endif
 
-#define run_car '1'   //按键前
-#define back_car '2'  //按键后
-#define left_car '3'  //按键左
-#define right_car '4' //按键右
-#define stop_car '0'  //按键停
+#define run_car '1'		   //按键前
+#define back_car '2'	   //按键后
+#define left_car '3'	   //按键左
+#define right_car '4'	  //按键右
+#define spin_left_car '5'  //按键左旋
+#define spin_right_car '6' //按键右旋
+#define stop_car '7'	   //按键停
+#define servoL 'L'		   //舵机左转
+#define servoR 'R'		   //舵机右转
+#define servoS 'S'		   //舵机停止
 
 #define ON 1  //使能LED
 #define OFF 0 //禁止LED
 
 /*小车运行状态枚举*/
 const typedef enum {
-	enSTOP = 0,
-	enRUN,
+	enRUN = 1,
 	enBACK,
 	enLEFT,
 	enRIGHT,
-	enTLEFT,
-	enTRIGHT
+	enSPINLEFT,
+	enSPINRIGHT,
+	enSTOP
 } enCarState;
+
+/*小车摄像头左右舵机状态枚举*/
+const typedef enum {
+	enServoL = 1,
+	enServoR,
+	enServoS
+} enServoState;
 
 const char enServo[] = {0, 1, 2, 3};
 
 const int key = 7; //按键key
 
 int flag = 0;
+int Servo_LR = 90;
 
 /*电压引脚及其变量设置*/
 const int VoltagePin = A2;
 double VoltageValue = 0;
 
 /*小车初始速度控制*/
-int CarSpeedControl = 150;
-
-/*颜色值*/
-int red, green, blue;
-
-/*计时变量用于延时*/
-int time = 20000;
-int count = 10;
+static int CarSpeedControl = 150;
 
 /*串口数据设置*/
 int IncomingByte = 0;			 //接收到的 data byte
 String InputString = "";		 //用来储存接收到的内容
 boolean NewLineReceived = false; //前一次数据结束标志
 boolean StartBit = false;		 //协议开始标志
-String ReturnTemp = "";			 //存储返回值
 /*状态机状态*/
-int g_CarState = enSTOP; //1前2后3左4右0停止
-int g_modeSelect = 0;	//0是默认状态;  1:红外遥控 2:巡线模式 3:超声波避障 4: 七彩探照 5: 寻光模式 6: 红外跟踪
-boolean g_motor = false;
+static int g_CarState = enSTOP;			//1前2后3左4右5左旋6右旋7停止
+static int g_ServoState = enServoS; //1左转2右转3停止
+
 
 /**
 * Function       setup
@@ -474,107 +479,102 @@ void serial_data_parse()
 {
 
 	//解析上位机发来的通用协议指令,并执行相应的动作
-	//$4WD,PTZ180#
+	//$4WD,UD180#
 	if (InputString.indexOf("4WD") > 0)
 	{
+		Serial.println(InputString);
 		//解析上位机发来的舵机云台的控制指令并执行舵机旋转
-		//如:$4WD,PTZ180# 舵机转动到180度
-		if (InputString.indexOf("PTZ") > 0)
+		//$4WD,UD180# 舵机转动到180度
+		if (InputString.indexOf("UD") > 0)
 		{
-			int i = InputString.indexOf("PTZ"); //寻找以PTZ开头,#结束中间的字符
+			int i = InputString.indexOf("UD"); //寻找以PTZ开头,#结束中间的字符
 			int ii = InputString.indexOf("#", i);
 			if (ii > i)
 			{
-				String m_skp = InputString.substring(i + 3, ii);
+				String m_skp = InputString.substring(i + 2, ii);
 				int m_kp = m_skp.toInt(); //将找到的字符串变成整型
-				Servo180(2, 180 - m_kp);  //转动到指定角度m_kp
-				InputString = "";		  //清空串口数据
-				NewLineReceived = false;
-				return;
+				// Servo180(3, 180 - m_kp);  //转动到指定角度m_kp
+				Servo180(3, 180 - m_kp); //转动到指定角度m_kp
+				delay(10);
+			}
+			InputString = ""; //清空串口数据
+			NewLineReceived = false;
+			return;
+		}
+
+		//解析上位机发来的舵机云台的控制指令并执行舵机旋转
+		//$4WD,LRS# 舵机停止转动
+		//$4WD,LRL# 舵机左转
+		//$4WD,LRR# 舵机右转
+		if (InputString.indexOf("LR") > 0)
+		{
+			switch (InputString[7])
+			{
+			case servoL:
+				g_ServoState = enServoL;
+				break;
+			case servoR:
+				g_ServoState = enServoR;
+				break;
+			case servoS:
+				g_ServoState = enServoS;
+				break;
 			}
 		}
+		InputString = ""; //清空串口数据
+		NewLineReceived = false;
+		return;
 	}
 	//解析上位机发来的通用协议指令,并执行相应的动作
-	//如:$1,0,0,0,0,0,0,0,0,0#    小车前进
+	//如:$1,0,0,0#    小车前进
 	if (InputString.indexOf("4WD") == -1)
 	{
-		//小车原地左旋右旋判断
-		if (InputString[3] == '1') //小车原地左旋
-		{
-			g_CarState = enTLEFT;
-		}
-		else if (InputString[3] == '2') //小车原地右旋
-		{
-			g_CarState = enTRIGHT;
-		}
-		else
-		{
-			g_CarState = enSTOP;
-		}
-
-		//小车鸣笛判断
-		if (InputString[5] == '1') //鸣笛
-		{
-		}
-		
-		//舵机左旋右旋判断
-		if (InputString[9] == '1') //舵机旋转到180度
-		{
-			Servo180(7, 180); //转动到指定角度
-		}
-
-		if (InputString[9] == '2') //舵机旋转到0度
-		{
-			Servo180(7, 0); //转动到指定角度
-		}
-
-		//舵机归为判断
-		if (InputString[17] == '1') //舵机旋转到90度
-		{
-			Servo180(7, 90); //转动到指定角度
-		}
-
 		//小车加减速判断
-		if (InputString[7] == '1') //加速，每次加50
+		if (InputString[2] == '1') //加速，每次加50
 		{
 			CarSpeedControl += 50;
-			if (CarSpeedControl > 255)
+			if (CarSpeedControl > 150)
 			{
-				CarSpeedControl = 255;
+				CarSpeedControl = 150;
 			}
+			return;
 		}
-		if (InputString[7] == '2') //减速，每次减50
+		if (InputString[2] == '2') //减速，每次减50
 		{
 			CarSpeedControl -= 50;
 			if (CarSpeedControl < 50)
 			{
 				CarSpeedControl = 50;
 			}
+			return;
 		}
-		//小车的前进,后退,左转,右转,停止动作
-		if (g_CarState != enTLEFT && g_CarState != enTRIGHT)
+
+		switch (InputString[1])
 		{
-			switch (InputString[1])
-			{
-			case run_car:
-				g_CarState = enRUN;
-				break;
-			case back_car:
-				g_CarState = enBACK;
-				break;
-			case left_car:
-				g_CarState = enLEFT;
-				break;
-			case right_car:
-				g_CarState = enRIGHT;
-				break;
-			case stop_car:
-				g_CarState = enSTOP;
-				break;
-			default:
-				g_CarState = enSTOP;
-				break;
-			}
+		case run_car:
+			g_CarState = enRUN;
+			break;
+		case back_car:
+			g_CarState = enBACK;
+			break;
+		case left_car:
+			g_CarState = enLEFT;
+			break;
+		case right_car:
+			g_CarState = enRIGHT;
+			break;
+		case spin_left_car:
+			g_CarState = enSPINLEFT;
+			break;
+		case spin_right_car:
+			g_CarState = enSPINRIGHT;
+			break;
+		case stop_car:
+			g_CarState = enSTOP;
+			break;
+		default:
+			g_CarState = enSTOP;
+			break;
 		}
 
 		InputString = ""; //清空串口数据
@@ -598,10 +598,10 @@ void serial_data_parse()
 		case enBACK:
 			back(CarSpeedControl);
 			break;
-		case enTLEFT:
+		case enSPINLEFT:
 			spin_left(CarSpeedControl);
 			break;
-		case enTRIGHT:
+		case enSPINRIGHT:
 			spin_right(CarSpeedControl);
 			break;
 		default:
@@ -612,10 +612,9 @@ void serial_data_parse()
 }
 /**
 * Function       loop
-* @author        liusen
-* @date          2017.07.25
-* @brief         先延时2，再前进1，后退1s,左转2s,右转2s,
-*                原地左转3s,原地右转3s,停止0.5s
+* @author        wusicaijuan
+* @date          2019.07.08
+* @brief         
 * @param[in]     void
 * @retval        void
 * @par History   无
@@ -634,6 +633,9 @@ void loop()
 		Serial.println(InputString);
 		serial_data_parse(); //调用串口解析函数
 	}
+
+	Servo_State();
+
 	voltage_test();
 }
 
@@ -737,6 +739,24 @@ void welcome()
 }
 
 /*
+* Function       Clear_All_PWM
+* @author        wusicaijuan
+* @date          2019.07.04
+* @brief         关闭PCA9685所有PWM
+* @param[in]     void
+* @param[out]    void
+* @retval        void
+* @par History   无
+*/
+void Clear_All_PWM()
+{
+	for (int i = 0; i < 16; i++)
+	{
+		pwm.setPWM(i, 0, 0);
+	}
+}
+
+/*
 * Function       Controling
 * @author        wusicaijuan
 * @date          2019.07.04
@@ -746,9 +766,30 @@ void welcome()
 * @retval        void
 * @par History   无
 */
-void Clear_All_PWM()
+void Servo_State()
 {
-	for(int i = 0; i < 16; i++){
-		pwm.setPWM(i, 0, 0);
+	if (g_ServoState != enServoS)
+	{
+		if (g_ServoState == enServoL)
+		{
+			Servo_LR++;
+			if (Servo_LR > 180)
+			{
+				Servo_LR = 180;
+			}
+			Servo180(2, Servo_LR);
+			delay(10);
+		}
+		if (g_ServoState == enServoR)
+		{
+			Servo_LR--;
+			if (Servo_LR < 0)
+			{
+				Servo_LR = 0;
+			}
+			Servo180(2, Servo_LR);
+			delay(10);
+		}
+		return;
 	}
 }
